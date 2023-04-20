9d9ef38d60f908db0bf317b175c35ebb80af1719
Carl Serquiña <serquinajhunlo@gmail.com>
2023-04-20 13:59:58 +0800

    feature/initial-features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="" className="logo">
+          My Blog
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2022/04/EF-DELTA-Pro-3.png?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+
+        <div className="texts">
+          <h2>
+            After ultracharged growth, battery maker EcoFlow comes for the
+            glampers
+          </h2>
+
+          <p className="info">
+            <a className="author">Carl Serquina</a>
+
+            <time>2023-01-06 16:45</time>
+          </p>
+
+          <p className="summary">
+            In recent years, episodes of extreme climate crises and power
+            outrages have driven doomsday preppers to plan for off-the-grid
+            survival scenarios. The mentality has been a boon to EcoFlow, a
+            Shenzhen-based power generating and storing unicorn, which racked up
+            $220 million in revenues last year as consumers in the U.S. demanded
+            its solar-powered portable power stations.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
